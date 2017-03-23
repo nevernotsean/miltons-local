7c352d671c588ec26e22e4ae3a6c5e9bba5e32e7
Sean Harrington <sean@never-not.com>
2017-03-23 09:58:04 -0400

    add pixel code
    Signed-off-by: Sean Harrington <sean@never-not.com>

--- wp-content/themes/miltons-nn/header.php
+++ wp-content/themes/miltons-nn/header.php
@@ -38,8 +38,24 @@
 		<meta name="theme-color" content="#ffffff">
 
 		<?php wp_head(); ?>
-		
+
 		<link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/css/foundation.css?ver=' . filemtime( get_template_directory() . '/css/foundation.css') ?>">
+
+		<!-- Facebook Pixel Code -->
+		<script>
+		!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+		n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+		n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+		t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+		document,'script','https://connect.facebook.net/en_US/fbevents.js');
+		fbq('init', '272178013222519'); // Insert your pixel ID here.
+		fbq('track', 'PageView');
+		</script>
+		<noscript><img height="1" width="1" style="display:none"
+		src="https://www.facebook.com/tr?id=272178013222519&ev=PageView&noscript=1"
+		/></noscript>
+		<!-- DO NOT MODIFY -->
+		<!-- End Facebook Pixel Code -->
 
 	</head>
 	<body <?php body_class('no-fouc'); ?> data-uri="<?php echo get_stylesheet_directory_uri(); ?>">
